aec2c8b9554c355c5f5a0f8f1f9d4f1eb8a377c7
as <>
2021-08-13 19:18:13 +0900

    2Blockの親を作成

--- Assets/MyScript/GameController.cs
+++ Assets/MyScript/GameController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject[] blocks;
+    public GameObject twinBlocks;
+    GameObject currentBlocks;
     
     void Start()
     {
@@ -18,9 +20,15 @@ public class GameController : MonoBehaviour
 
     void CreateBlocks()
     {
+        currentBlocks = Instantiate(twinBlocks);
+        currentBlocks.transform.position = new Vector3(2, 11, 0);
+
         GameObject block1 = Instantiate(blocks[Random.Range(0, 4)]);
         block1.transform.position = new Vector3(2, 11, 0);
+        block1.transform.SetParent(currentBlocks.transform, true);
+
         GameObject block2 = Instantiate(blocks[Random.Range(0, 4)]);
         block2.transform.position = new Vector3(2, 12, 0);
+        block2.transform.SetParent(currentBlocks.transform, true);
     }
 }
