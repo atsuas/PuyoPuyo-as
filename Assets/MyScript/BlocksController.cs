3998dc82870fa4b6b7d41f4adfc4ad5604638799
as <>
2021-08-13 19:44:29 +0900

    左右、下、回転を実装

---
+++ Assets/MyScript/BlocksController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.gameObject.transform.position += new Vector3(0, -1, 0);
+        }
+        //回転
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), 90);
+        }
+    }
+}
--- Assets/MyScript/GameController.cs
+++ Assets/MyScript/GameController.cs
@@ -20,15 +20,20 @@ public class GameController : MonoBehaviour
 
     void CreateBlocks()
     {
+        //親を取得して位置指定
         currentBlocks = Instantiate(twinBlocks);
         currentBlocks.transform.position = new Vector3(2, 11, 0);
 
+        //ブロック1を取得して位置指定
         GameObject block1 = Instantiate(blocks[Random.Range(0, 4)]);
         block1.transform.position = new Vector3(2, 11, 0);
+        //currentBlocks(twinBlocks)と親子関係にする
         block1.transform.SetParent(currentBlocks.transform, true);
 
+        //ブロック2を取得して位置指定
         GameObject block2 = Instantiate(blocks[Random.Range(0, 4)]);
         block2.transform.position = new Vector3(2, 12, 0);
+        //currentBlocks(twinBlocks)と親子関係にする
         block2.transform.SetParent(currentBlocks.transform, true);
     }
 }
