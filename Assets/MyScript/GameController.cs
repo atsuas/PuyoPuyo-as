2365f79eda26d522ab2e4cebfe598b1adfe03f2a
as <>
2021-08-13 19:06:56 +0900

    Blockを表示する、位置指定

---
+++ Assets/MyScript/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] blocks;
+    
+    void Start()
+    {
+        CreateBlocks();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void CreateBlocks()
+    {
+        GameObject block1 = Instantiate(blocks[Random.Range(0, 4)]);
+        block1.transform.position = new Vector3(2, 11, 0);
+        GameObject block2 = Instantiate(blocks[Random.Range(0, 4)]);
+        block2.transform.position = new Vector3(2, 12, 0);
+    }
+}
