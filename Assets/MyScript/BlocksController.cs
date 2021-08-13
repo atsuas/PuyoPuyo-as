e61d2216c20d614ea1d404a88f311a8d2cdb0e51
as <>
2021-08-13 20:12:43 +0900

    動ける範囲を指定

--- Assets/MyScript/BlocksController.cs
+++ Assets/MyScript/BlocksController.cs
@@ -15,24 +15,63 @@ public class BlocksController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.gameObject.transform.position += new Vector3(-1, 0, 0);
+            //右に+1して動けなくする
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(1, 0, 0);
+            }
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             this.gameObject.transform.position += new Vector3(1, 0, 0);
+            //左に-1して動けなくする
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(-1, 0, 0);
+            }
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             this.gameObject.transform.position += new Vector3(0, -1, 0);
+            //上に+1して動けなくする
+            if (!CanMove())
+            {
+                this.gameObject.transform.position += new Vector3(0, 1, 0);
+            }
         }
         //回転
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), 90);
             //回転しても向きを同じにする
-            foreach (Transform childBlock in transform)
+            foreach (Transform childBlocks in transform)
             {
-                childBlock.transform.RotateAround(childBlock.transform.position, new Vector3(0, 0, 1), -90);
+                childBlocks.transform.RotateAround(childBlocks.transform.position, new Vector3(0, 0, 1), -90);
+            }
+            if (!CanMove())
+            {
+                //反対回転させて回転できなくする
+                this.gameObject.transform.RotateAround(transform.position, new Vector3(0, 0, 1), -90);
+                foreach (Transform childBlocks in transform)
+                {
+                    childBlocks.transform.RotateAround(childBlocks.transform.position, new Vector3(0, 0, 1), 90);
+                }
             }
         }
+    }
+
+    //動ける範囲を指定
+    bool CanMove()
+    {
+        foreach (Transform childBlocks in transform)
+        {
+            int X = Mathf.RoundToInt(childBlocks.transform.position.x);
+            int Y = Mathf.RoundToInt(childBlocks.transform.position.y);
+            if (X < 0 || X > 5 || Y < 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
